ef042ab7ad958bbf0bbc79de2e775007c014d3ae
DaoTrongHieuk2 <hieubeo6944@gmail.com>
2023-10-07 08:05:47 +0700

    MVC Tinh Luong

---
+++ DemoMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+using DemoMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+public class EmployeeController : Controller
+{
+    public IActionResult IndexEmployee()
+    {
+        return View();
+    }
+    [HttpPost]
+
+    public IActionResult IndexEmployee(string HoTen, decimal LuongCoBan, decimal HeSoLuong, decimal PhuCap)
+    {
+        decimal luong = LuongCoBan * HeSoLuong + PhuCap;
+        string tl = HoTen + "-" + luong;
+        ViewBag.thongbao = tl;
+        return View();
+    }
+
+
+
+
+}
