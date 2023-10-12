19518e3c4b5835ea56588106a21c376c13fbee75
DaoTrongHieuk2 <hieubeo6944@gmail.com>
2023-10-12 20:34:12 +0700

    MVC GiaiPtrinhBacHai update

--- DemoMVC/Controllers/PTBacHaiController.cs
+++ DemoMVC/Controllers/PTBacHaiController.cs
@@ -10,32 +10,7 @@ public class PTBacHaiController : Controller
     }
     [HttpPost]
 
-    /* public IActionResult IndexPTBacHai(double a, double b, double c, double delta, double x1, double x2)
-    {
-        GiaiPTBac2 model = new GiaiPTBac2();
-        model.a = a;
-        model.b = b;
-        model.c = c;
-        delta = b * b - 4 * a * c;
-        if (delta > 0)
-        {
-            System.Console.WriteLine("Phuong trinh co 2 nghiem phan biet:");
-            model.x1 = (-b + Math.Sqrt(delta)) / (2 * a);
-            model.x2 = (-b - Math.Sqrt(delta)) / (2 * a);
-        }
-        else if (delta == 0)
-        {
-            System.Console.WriteLine("Phuong trinh co nghiem:");
-            model.x1 = x2 = -b / (2 * a);
-        }
-        else
-        {
-            model.x1 = x2 = double.NaN;
-            System.Console.WriteLine("Phuong trinh vo nghiem");
-        }
-        ViewBag.thongbao = model;
 
-        return View(); */
     public IActionResult IndexPTBacHai(GiaiPTBac2 model)
     {
         double delta = model.b * model.b - 4 * model.a * model.c;
